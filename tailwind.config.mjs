74ee0634f53fd9afe967e525a8da86ca9c341011
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2024-08-02 23:23:58 -0300

    estilizando navbar utilizando biblioteca daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			'pink-dark': '#7D298A',
+			'white': '#FFFFFF',
 		extend: {},
 	},
-	plugins: [],
+},
+	plugins: [
+		require('daisyui'),
+	],
 }
+
+   
